91e63633721d46838984c2182708ec8e18bcbe08
amattn <amattn@users.noreply.github.com>
2021-04-28 13:29:20 -0700

    tagging version v0.3.2

--- pkg/chremotelib/version.go
+++ pkg/chremotelib/version.go
@@ -7,9 +7,9 @@ import (
 
 const (
 	internalIdentifier           = "github.com/amattn/chremotelib"
-	internalBuildTimestamp int64 = 1619641715
-	internalBuildNumber    int64 = 42
-	internalVersionString        = "0.3.1"
+	internalBuildTimestamp int64 = 1619641760
+	internalBuildNumber    int64 = 44
+	internalVersionString        = "0.3.2"
 )
 
 func BuildDate() time.Time {
